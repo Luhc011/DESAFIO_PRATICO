513d995f7f6af7cdaad1779a58555da023e5e14b
Lucas H <lucashcardoso26@gmail.com>
2023-11-23 14:21:18 -0300

    Migrations add

---
+++ src/DESAFIO_PRATICO.API/Migrations/20231123165144_UserTaskVinc.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DESAFIO_PRATICO.API.Migrations
+{
+    public partial class UserTaskVinc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Task",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_UserId",
+                table: "Task",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_User_UserId",
+                table: "Task",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_User_UserId",
+                table: "Task");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Task_UserId",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Task");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
