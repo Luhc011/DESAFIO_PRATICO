e7b27e1bba8f6c10777949fa46e59b7a378d779e
Lucas H <lucashcardoso26@gmail.com>
2023-11-23 14:20:09 -0300

    Controllers done

---
+++ src/DESAFIO_PRATICO.API/Controllers/TaskController.cs
@@ -0,0 +1,55 @@
+﻿using DESAFIO_PRATICO.API.DataContexts.Repositories.Contracts;
+using DESAFIO_PRATICO.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DESAFIO_PRATICO.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskController(ITaskRepository taskRepository) => _taskRepository = taskRepository;
+
+        [HttpGet]
+        public async Task<ActionResult<TaskModel>> GetTasks()
+        {
+            var tasks = await _taskRepository.GetAllAsync();
+
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> GetTaskById(int id)
+        {
+            var task = await _taskRepository.GetByIdAsync(id);
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> CreateTask(TaskModel task)
+        {
+            var taskCreated = await _taskRepository.CreateAsync(task);
+
+            return CreatedAtAction(nameof(GetTaskById), new { id = taskCreated.Id }, taskCreated);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TaskModel>> UpdateTask(TaskModel task, int id)
+        {
+            var taskUpdated = await _taskRepository.UpdateAsync(task, id);
+
+            return Ok(taskUpdated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteTask(int id)
+        {
+            var taskDeleted = await _taskRepository.DeleteAsync(id);
+
+            return Ok(taskDeleted);
+        }
+    }
+}
---
+++ src/DESAFIO_PRATICO.API/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using DESAFIO_PRATICO.API.DataContexts.Repositories.Contracts;
+using DESAFIO_PRATICO.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DESAFIO_PRATICO.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository) => _userRepository = userRepository;
+
+        [HttpGet]
+        public async Task<ActionResult<UserModel>> GetUsers()
+        {
+            var users = await _userRepository.GetAllAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> GetUserById(int id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+
+            return Ok(user);
+        }
+
+        [HttpGet("name/{username}")]
+        public async Task<ActionResult<UserModel>> GetUserByName(string username)
+        {
+            var user = await _userRepository.GetByNameAsync(username);
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> CreateUser(UserModel user)
+        {
+            var userCreated = await _userRepository.CreateAsync(user);
+
+            return CreatedAtAction(nameof(GetUserById), new { id = userCreated.Id }, userCreated);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>> UpdateUser(UserModel user, int id)
+        {
+            var userUpdated = await _userRepository.UpdateAsync(user, id);
+
+            return Ok(userUpdated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteUser(int id)
+        {
+            var userDeleted = await _userRepository.DeleteAsync(id);
+
+            return Ok(userDeleted);
+        }
+    }
+}
