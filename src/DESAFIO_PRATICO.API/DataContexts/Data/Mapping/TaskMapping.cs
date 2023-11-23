b856fb8476e31e31b188132f46d86e59a509f43f
Lucas H <lucashcardoso26@gmail.com>
2023-11-23 14:20:34 -0300

    mappings done

---
+++ src/DESAFIO_PRATICO.API/DataContexts/Data/Mapping/TaskMapping.cs
@@ -0,0 +1,37 @@
+﻿using DESAFIO_PRATICO.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DESAFIO_PRATICO.API.DataContexts.Data.Mapping
+{
+    public class TaskMapping : IEntityTypeConfiguration<TaskModel>
+    {
+        public void Configure(EntityTypeBuilder<TaskModel> builder)
+        {
+            builder.ToTable("Task");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=> x.Title)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x=> x.Description)
+                .IsRequired()
+                .HasMaxLength(500)
+                .HasColumnType("varchar(500)");
+
+            builder.Property(x=> x.Status)
+                .IsRequired()
+                .HasColumnType("int");
+
+            builder.Property(x => x.UserId)
+                .HasColumnType("int"); 
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ src/DESAFIO_PRATICO.API/DataContexts/Data/Mapping/UserMapping.cs
@@ -0,0 +1,25 @@
+﻿using DESAFIO_PRATICO.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DESAFIO_PRATICO.API.DataContexts.Data.Mapping
+{
+    public class UserMapping : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=> x.Name)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x=> x.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+        }
+    }
+}
