6f00aeb2c9a7dce3b001a71813432d23f14a48f9
Lucas H <lucashcardoso26@gmail.com>
2023-11-23 14:21:25 -0300

    Models

---
+++ src/DESAFIO_PRATICO.API/Models/TaskModel.cs
@@ -0,0 +1,27 @@
+﻿using DESAFIO_PRATICO.API.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DESAFIO_PRATICO.API.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Title field is required.")]
+        [StringLength(100, ErrorMessage = "The Title field cannot exceed 100 characters.")]
+        public string? Title { get; set; }
+
+        [StringLength(500, ErrorMessage = "The Description field cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The Status field is required.")]
+        public StatusTasks Status { get; set; }
+
+        public int? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public UserModel? User { get; set; } 
+
+    }
+}
---
+++ src/DESAFIO_PRATICO.API/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DESAFIO_PRATICO.API.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The field {0} must be between {2} and {1} characters")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in invalid format")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The field {0} must be between {2} and {1} characters")]
+        public string? Email { get; set; }
+    }
+}
