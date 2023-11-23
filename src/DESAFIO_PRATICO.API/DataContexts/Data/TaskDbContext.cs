84b6c160dd59e6a280f2bd98e18d45a571ff3109
Lucas H <lucashcardoso26@gmail.com>
2023-11-23 14:21:07 -0300

    db context add

---
+++ src/DESAFIO_PRATICO.API/DataContexts/Data/TaskDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DESAFIO_PRATICO.API.DataContexts.Data.Mapping;
+using DESAFIO_PRATICO.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DESAFIO_PRATICO.API.DataContexts.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel>? Users { get; set; } = null;
+        public DbSet<TaskModel>? Tasks { get; set; } = null;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TaskMapping());
+            modelBuilder.ApplyConfiguration(new UserMapping());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskDbContext).Assembly);
+        }
+    }
+}
